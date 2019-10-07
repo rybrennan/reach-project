d6c4c32e1467ca7951b78e81861f4e4b7807cd82
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-10-07 06:05:44 -0700

    [setup] react and express working with webpack and babel

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000
+//app.get('/', (req,res) => res.send("HELLO FROM EXPRESS"));
+
+app.use(express.static('public'));
+
+app.listen(PORT,  () => console.log(`Express Server listening on port ${PORT}!`));
---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class App extends React.Component {
+  render() {
+    return <h3>Testing from React Woop Woop!</h3>;
+  }
+}

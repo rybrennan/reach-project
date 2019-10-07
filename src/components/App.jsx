b74c1884d0df2f5d5bc62e3860c03f58bde3b1d7
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-10-07 06:14:47 -0700

    [test] Changing state is not causing errors  Please enter the commit message for your changes. Lines starting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,10 +8,22 @@ class App extends React.Component {
       secretWord: 'Ryan',
       incorrectLetters: []
     }
+
+    this.handleClearAll = this.handleClearAll.bind(this);
   }
+
+  handleClearAll() {
+    this.setState({
+      secretWord: ''
+    })
+  }
+
   render() {
-    return(
+    return (
       <div className="App">
+        <div>
+          <button onClick={() => this.handleClearAll()}>Clear All</button>
+        </div>
         <h1> {this.state.secretWord} </h1>
       </div>
     );

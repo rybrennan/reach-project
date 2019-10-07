4a5f0443db746a82de2b4ee5f20d94368d502368
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-10-07 06:18:55 -0700

    Fetching from the Linkedin API

---
+++ utils/index.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const url = 'http://app.linkedin-reach.io/words?difficulty=5&count=1';
+
+const http = axios.create({
+  baseURL: url
+});
+
+
+const getAll = (callback) => {
+  http.get(url)
+  .then((response) => {
+
+    callback(null, response.data);
+  })
+  .catch((error) => {
+    console.log(error)
+    callback(error, null);
+  })
+}
+
+module.exports = {
+  getAll,
+  http
+};
+
+getAll((err, results) => {
+  console.log('I am results', results)
+})

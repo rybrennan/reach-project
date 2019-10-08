1aa7c2c15e4850b22d90aa6785775aa80d35ad1f
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-10-07 19:50:14 -0700

    [wip] install bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx'
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />,document.getElementById('app'));
 
